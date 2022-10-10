0da11b041559d260d0409451d29392a280dc3033
kevinmi03 <71861116+kevinmi03@users.noreply.github.com>
2022-10-10 17:44:43 +0700

    Add files via upload

---
+++ identitas.php
@@ -0,0 +1,11 @@
+<?php
+$nama = "Dionisius Kelvianus Mite Me";
+$umur = 28;
+$asalSekolah ='SMA NEGERI 1 WAIKABUBAK';
+$semester = 3;
+$jurusan ="Sistem Informasi";
+
+	echo "Hello, perkenalkan nama saya ".$nama.", umur
+		  saya ".$umur." tahun. Sekarang saya kuliah di ".
+		  $asalPerguruanTinggi.", semester ".$semester." jurusan ".$jurusan;
+	?>
